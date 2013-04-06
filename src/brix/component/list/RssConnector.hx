9b9b54326bbecf14b81d8eadde6afe939c9c0fe4
codam <raphael.harmel@gmail.com>
2013-04-06 20:02:06 +0200

    rss connector update

--- src/brix/component/list/RssConnector.hx
+++ src/brix/component/list/RssConnector.hx
@@ -19,8 +19,8 @@ import js.Lib;
 
 class RssConnector extends ConnectorBase
 {
-	//private var processVal:Bool = false;
-	private static var getThumbImage:Bool = true;
+	// process values switch
+	private var processVal:Bool = false;
 	
 	/**
 	 * Parse RSS String to object
@@ -39,7 +39,7 @@ class RssConnector extends ConnectorBase
 	 * @param	rss
 	 * @return
 	 */
-	public static function rss2object(rss:Xml):Dynamic
+	public function rss2object(rss:Xml):Dynamic
 	{
 		// init items Array
 		var items:Array<Dynamic> = new Array<Dynamic>();
@@ -68,7 +68,10 @@ class RssConnector extends ConnectorBase
 							var key:String = itemParam.nodeName;
 							var value:String = itemParam.firstChild().nodeValue;
 							
-							value = processValue(item, key, value);
+							if (processVal)
+							{
+								value = processValue(item, key, value);
+							}
 							
 							Reflect.setField(item, key, value);
 						//}
@@ -78,7 +81,7 @@ class RssConnector extends ConnectorBase
 						
 					}
 				}
-				trace(item);
+				//trace(item);
 				// add item to item array
 				items.push(item);
 			}
@@ -89,89 +92,15 @@ class RssConnector extends ConnectorBase
 	
 	/**
 	 * Process the value of a key. The process depends on the key.
+	 * to be overriden in children classes
 	 * 
 	 * @param	key
 	 * @param	value
 	 * @return
 	 */
-	private static function processValue(item:Dynamic, key:String, value:String):String
+	private function processValue(item:Dynamic, key:String, value:String):String
 	{
-		switch(key)
-		{
-			case "description":
-				// unescape html
-				value = StringTools.htmlUnescape(value);
-				
-				// get thumb
-				if (getThumbImage)
-				{
-					//trace(value);
-					//value = getThumb(StringTools.htmlUnescape(value));
-					//trace(value);
-					Reflect.setField(item, "image", getThumb(StringTools.htmlUnescape(value)));
-				}
-		}
-		
 		return value;
-	}
-	
-	/**
-	 * Gets the thumb image, i.e. the first image in the html string
-	 * 
-	 * @param	htmlString
-	 */
-	static private function getThumb(htmlString:String):String
-	{
-		// get thumbnail from description
-		var imgNodeStartIndex:Int = htmlString.indexOf("<img ");
-		var imgNode:String = "";
-		var imgUrl:String = "";
-		var imgUrlStartIndex:Int = 0;
-		
-		// if img node name has been found
-		if ( imgNodeStartIndex != -1)
-		{
-			// get img node content
-			htmlString = htmlString.substr(imgNodeStartIndex);
-			var imgNodeEndIndex:Int = htmlString.indexOf(">") + 1;
-			imgNode = htmlString.substr(0, imgNodeEndIndex);
-			
-			// get image url
-			var srcKeyWord:String = 'src=';
-			imgUrlStartIndex = imgNode.indexOf(srcKeyWord);
-			
-			// if srcKeyWord string has been found
-			if (imgUrlStartIndex != -1)
-			{
-				// get the delimitor
-				var imgUrlDelimitor:String = imgNode.substr(imgUrlStartIndex + srcKeyWord.length, 1);
-				
-				// get the image url
-				imgUrl = imgNode.substr(imgUrlStartIndex + srcKeyWord.length + 1);
-				var imgUrlEndIndex:Int = imgUrl.indexOf(imgUrlDelimitor);
-				imgUrl = imgUrl.substr(0, imgUrlEndIndex);
-				
-				return imgUrl;
-			}
-		}
-		// workaround for silicon sentier feed: if no img field, get the thumb from description only
-		else
-		{
-			imgUrlStartIndex = htmlString.indexOf("<p>http://");
-			if ( imgUrlStartIndex != -1)
-			{
-				// get image url
-				var srcKeyWord:String = '<p>';
-				imgUrl = htmlString.substr(imgUrlStartIndex + srcKeyWord.length);
-				var imgUrlEndIndex:Int = imgUrl.indexOf("</p>");
-				imgUrl = imgUrl.substr(0, imgUrlEndIndex);
-				
-				return imgUrl;
-			}
-		}
-		
-		return "";
-		
 	}
 	
 }
